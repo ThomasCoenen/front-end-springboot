ff5903a4c7338a301136c742dca2327220b78d9e
ThomasCoenen <thomas.coenen58@gmail.com>
2021-08-02 01:26:16 -0500

    email validation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,6 @@ function Footer() {
     return (
         <div>
             <footer className='footer'>
-                <span className='text-muted'>All rights reserved</span>
             </footer>
         </div>
     )
